c106797e97c713d98b0d092e6ede2b24d9d0886d
Xedoken <kubabuba103@gmail.com>
2023-11-15 23:50:33 +0100

    Add project files.

---
+++ DeathModeClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using MelonLoader;
+using HarmonyLib;
+
+namespace DeathMode
+{
+    public class DeathModeClass : MelonMod
+    {
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            LoggerInstance.Msg($"Scene {sceneName} with build index {buildIndex} has been loaded!");
+            if (buildIndex == -1)
+            {
+                LoggerInstance.Msg($"You are in game right now");
+            }
+        }
+    }
+}
